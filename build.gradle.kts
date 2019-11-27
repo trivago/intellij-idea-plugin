b6c6778459256a72caa7b8b77e05b66d7d27b332
aferracuti <adriano.ferracuti@trivago.com>
2019-11-27 11:19:22 +0100

    Project skeleton

---
+++ build.gradle.kts
@@ -0,0 +1,38 @@
+plugins {
+    id("org.jetbrains.intellij") version "0.4.14"
+    java
+    kotlin("jvm") version "1.3.60"
+}
+
+group = "com.trivago"
+version = "0.1"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+    testCompile("junit", "junit", "4.12")
+}
+
+// See https://github.com/JetBrains/gradle-intellij-plugin/
+intellij {
+    version = "2019.2.3"
+}
+configure<JavaPluginConvention> {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+}
+tasks {
+    compileKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+    compileTestKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+}
+tasks.getByName<org.jetbrains.intellij.tasks.PatchPluginXmlTask>("patchPluginXml") {
+    changeNotes("""
+      Add change notes here.<br>
+      <em>most HTML tags may be used</em>""")
+}
